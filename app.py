fa122e94878aa74038152e580dcccd9568544b75
gabriel.mascarenhas <gabriel.mascarenhas@intelie.com.br>
2016-06-06 16:20:31 -0300

    pizza

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ tasks = [
     {
         'id': 1,
         'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol, Pizza', 
         'done': False
     },
     {
